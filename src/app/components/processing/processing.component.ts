1a8bf8f805f2f5fd30d816d0db8f8a6244b4a489
jose angel <sepinaco@gmail.com>
2020-01-29 20:58:53 +0100

    processing funcionandogit add -A (falta ordenar el codigo)

--- src/app/components/processing/processing.component.ts
+++ src/app/components/processing/processing.component.ts
@@ -1,4 +1,8 @@
+// Thanks to https://stackoverflow.com/questions/49472433/how-to-use-p5-js-in-angular-5-application/53464162
+
 import { Component, OnInit } from '@angular/core';
+
+import { ProcessingService } from '../../services/processing.service';
 
 @Component({
   selector: 'app-processing',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProcessingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private processingService: ProcessingService) { }
 
   ngOnInit() {
+    
+    this.processingService.start();
+    //this.createCanvas();
   }
+
+  // private createCanvas(): void {
+  //   this.p5 = new P5(this.sketch);
+  // }
+
+  // private sketch(p: any) {
+  //   p.setup = () => {
+  //     p.createCanvas(700, 600);
+  //   };
+
+  //   p.draw = () => {
+  //     p.background(255);
+  //     p.fill(0);
+  //     p.rect(p.width / 2, p.height / 2, 50, 50);
+  //   };
+  // }
 
 }
